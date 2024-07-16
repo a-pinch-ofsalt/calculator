502421f0b1a967e684b1f3621998eb96997dfe98
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-07-16 09:11:18 -0500

    refactor: Removed debugging lines and extracted functions
    Removed 'alert' statements in operation functions' definitions. Created three smaller functions to extract functionality from all buttons' click events into more readable code.

--- javascript.js
+++ javascript.js
@@ -12,22 +12,28 @@ let operator = '';
 let thereIsExpression = false;
 
 const operations = {
-    '+': (num1, num2) => {
-        alert(`${num1} + ${num2}`);
-        return num1 + num2
-    },
-    '-': (num1, num2) => {
-        alert(`${num1} - ${num2}`);
-        return num1 - num2
-    },
-    '×': (num1, num2) => {
-        alert(`${num1} * ${num2}`);
-        return num1 * num2
-    },
-    '÷': (num1, num2) => {
-        alert(`${num1} / ${num2}`);
-        return num1 / num2
-    },
+    '+': (num1, num2) => {return num1 + num2},
+    '-': (num1, num2) => {return num1 - num2},
+    '×': (num1, num2) => {return num1 * num2},
+    '÷': (num1, num2) => {return num1 / num2}
+}
+
+const calculate = () => {
+    num1 = operations[operator](Number(num1), Number(num2));
+    display.textContent = num1;
+    num2 = '';
+}
+
+const clearCalculator = () => {
+    thereIsExpression = false;
+    operator = '';
+    num1 = '';
+    num2 = '';
+    display.textContent = '';
+}
+
+const typeIn = (text) => {
+    display.textContent += text;
 }
 
 for (const button of buttons.numpad) {
@@ -37,36 +43,28 @@ for (const button of buttons.numpad) {
         } else {
             num1 += button.textContent;
         }
-        display.textContent += button.textContent;
+        typeIn(button.textContent)
     });
 }
 
 for (const button of buttons.operator) {
     button.addEventListener('click', () => {
         if (thereIsExpression) {
-            num1 = operations[operator](Number(num1), Number(num2));
-            display.textContent = num1;
-            num2 = '';
+            calculate();
         } else {
             thereIsExpression = true;
         }
         operator = button.textContent;
-        display.textContent += button.textContent;
+        typeIn(button.textContent);
     });
 }
 
 buttons.equals.addEventListener('click', () => {
-    num1 = operations[operator](Number(num1), Number(num2));
-    display.textContent = num1;
-    num2 = '';
+    calculate();
     thereIsExpression = false;
 });
 
 buttons.clear.addEventListener('click', () => {
-    thereIsExpression = false;
-    operator = '';
-    num1 = '';
-    num2 = '';
-    display.textContent = '';
+    clearCalculator();
 });
 
